39cc5263ec822eac617edead033a45673f0487be
Brandan-Lee <brandan.sherbrooke@gmail.com>
2023-08-31 10:50:03 +0200

    Design of forms and connecting them

---
+++ Oak Academy Course/Projects/Stock Tracing App with N-Tier Architecture, Facade , Design Pattern and Entity Framework/StockTracking/FrmSalesList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockTracking
+{
+    public partial class FrmSalesList : Form
+    {
+        public FrmSalesList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = General.IsNumber(e);
+        }
+
+        private void txtSalesAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = General.IsNumber(e);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ChangeToSalesForm();
+        }
+
+        void ChangeToSalesForm()
+        {
+            FrmSales frm = new FrmSales();
+            this.Hide();
+            frm.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ChangeToSalesForm();
+        }
+    }
+}
